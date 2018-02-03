b65adc219fee28e3157d46c5077354a90b6fc6cd
badpaybad <badpaybad@gmail.com>
2018-02-03 12:55:07 +0700

    CheckinAndAutoNumberWorkfollow

--- DomainDrivenDesign.Domain/Workfollow/CheckinAndAutoNumberWorkfollow.cs
+++ DomainDrivenDesign.Domain/Workfollow/CheckinAndAutoNumberWorkfollow.cs
@@ -13,10 +13,13 @@ namespace DomainDrivenDesign.Domain.Workfollow
     /// <summary>
     /// scenario if Checkin created, so should create AutoNumber
     /// </summary>
-   public class CheckinAndAutoNumberWorkfollow:IEventHandle<CheckinCreated>
+    public class CheckinAndAutoNumberWorkfollow : IEventHandle<CheckinCreated>
     {
         public void Handle(CheckinCreated e)
         {
+            // use can make thin query facade to db read or blabla thing ... 
+            // but in here should not write to db directly. 
+            // call command if want to ask other domain do their action
             if (e.Duration >= 0)
             {
                 MemoryMessageBuss.PushCommand(new CreateAutoNumberTest($"Checkin created with Id: {e.Id} duration: {e.Duration}"));
