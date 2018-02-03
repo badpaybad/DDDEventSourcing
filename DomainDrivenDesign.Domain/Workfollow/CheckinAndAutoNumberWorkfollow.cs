bf24a2af9b1b5c876dc65cfcd314ef4dff0f5025
badpaybad <badpaybad@gmail.com>
2018-02-03 12:26:32 +0700

    Workfollow able do easily in CQRS

---
+++ DomainDrivenDesign.Domain/Workfollow/CheckinAndAutoNumberWorkfollow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainDrivenDesign.Core.Events;
+using DomainDrivenDesign.Core.Implements;
+using DomainDrivenDesign.Domain.AutoNumber.Commands;
+using DomainDrivenDesign.Domain.Events;
+
+namespace DomainDrivenDesign.Domain.Workfollow
+{
+    /// <summary>
+    /// scenario if Checkin created, so should create AutoNumber
+    /// </summary>
+   public class CheckinAndAutoNumberWorkfollow:IEventHandle<CheckinCreated>
+    {
+        public void Handle(CheckinCreated e)
+        {
+            if (e.Duration >= 0)
+            {
+                MemoryMessageBuss.PushCommand(new CreateAutoNumberTest($"Checkin created with Id: {e.Id} duration: {e.Duration}"));
+            }
+        }
+    }
+}
--- DomainDrivenDesign.TestDomain/Program.cs
+++ DomainDrivenDesign.TestDomain/Program.cs
@@ -23,15 +23,15 @@ namespace DomainDrivenDesign.TestDomain
             var checkin = new CreateCheckin(Guid.NewGuid(), 0, 10, DateTime.Now);
             MemoryMessageBuss.PushCommand(checkin);
 
-            Console.WriteLine("Employee commend");
-            MemoryMessageBuss.PushCommand(new CommentCheckinByEmployee(checkin.CheckinId, "Hi boss", 0));
+            //Console.WriteLine("Employee commend");
+            //MemoryMessageBuss.PushCommand(new CommentCheckinByEmployee(checkin.CheckinId, "Hi boss", 0));
 
-            Console.WriteLine("Evaluator comment");
-            MemoryMessageBuss.PushCommand(new CommentCheckinByEvaluator(checkin.CheckinId, "Hi man, we should change 'boss' to 'bro' ", 0));
+            //Console.WriteLine("Evaluator comment");
+            //MemoryMessageBuss.PushCommand(new CommentCheckinByEvaluator(checkin.CheckinId, "Hi man, we should change 'boss' to 'bro' ", 0));
             
 
-            Console.WriteLine("Test auto number");
-             MemoryMessageBuss.PushCommand(new CreateAutoNumberTest("My old Name"));
+            //Console.WriteLine("Test auto number");
+            // MemoryMessageBuss.PushCommand(new CreateAutoNumberTest("My old Name"));
             //from ui got Id for AutoNumberTest
             //using (var db =new TestDbContext())
             //{
